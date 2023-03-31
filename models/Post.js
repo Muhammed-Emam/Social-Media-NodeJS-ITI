446b3f2da1f77f39c405ebd40a480cd3379fd5b4
Muhammed-Emam <118661263+Muhammed-Emam@users.noreply.github.com>
2023-03-31 22:05:56 +0200

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,19 +1,27 @@
+// import Mongoose for MongoDB interaction
 const mongoose = require('mongoose');
+
+// assign Schema constructor from mongoose to a variable
 const Schema = mongoose.Schema;
 
-
+// define post schema using the Schema constructor
 const postSchema = new Schema({
-		Desc:{
-			type:String,
-			required:true,
-			unique:true
-		},
-		user:{
-			type:Schema.Types.ObjectId,
-			required:true,
-			ref:'User'
-		}
+    // post description string is required and must be unique
+    Desc:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    // user id as Object ID, which is required and references the User collection
+    user:{
+        type:Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
+    }
 })
 
+// create Post model from post schema with mongoose.model()
 const Post = mongoose.model('Post',postSchema);
+
+// export Post model so that it can be used in other modules
 module.exports = Post;
