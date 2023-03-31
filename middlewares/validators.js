112e234ee5db63375bffc92c729f101e55dfe0a1
Muhammed-Emam <118661263+Muhammed-Emam@users.noreply.github.com>
2023-03-31 22:02:29 +0200

    Update validators.js

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -1,20 +1,26 @@
+// import Joi for schema validation
 const Joi = require("joi");
 
-
+// define schema for login validation using Joi
 const loginSchema = Joi.object({
-	username: Joi.string().min(3).max(30).required(),
-	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    username: Joi.string().min(3).max(30).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
 })
 
+// middleware function to validate sign-in credentials
 const validateSignin = (req,res,next)=>{
-	const {error} = loginSchema.validate(req.body);
-	if(error){
-		const err = new Error(error.details[0].message);
-		err.statusCode = 400;
-		return next(err)
-	}
-	next();
+    // use the loginSchema to validate request body
+    const {error} = loginSchema.validate(req.body);
+    if(error){
+        // if error exists, throw an error with status code 400
+        const err = new Error(error.details[0].message);
+        err.statusCode = 400;
+        return next(err)
+    }
+    next();
 }
+
+// export validateSignin middleware function
 module.exports = {
-	validateSignin
+    validateSignin
 }
