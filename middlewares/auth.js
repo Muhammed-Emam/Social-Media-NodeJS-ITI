35e73e0036af8e3b15a716b2df65d13784976253
Muhammed-Emam <118661263+Muhammed-Emam@users.noreply.github.com>
2023-03-31 22:00:59 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,47 +1,54 @@
+// import required modules/packages
 const jwt = require('jsonwebtoken');
 const {promisify} = require('util');
 const User = require('../models/user');
 const {jwtSecret} = require('../config/config.env')
 const customError = require('../utils/customError');
+// promisify jsonwebtoken verify function
 const verifyJwt = promisify(jwt.verify);
 
+// protect routes by verifying token and attaching user to request object
 exports.protect= async (req,res,next)=>{
-	try{
-		//  extract token from headers
-		const token = req.headers.authorization;
-		if(!token){
-			const error = new Error('unauthorized');
-			error.statusCode = 401;
-			return next(error)
-		}
-		//  verify the token (secret)
-		const {id} = await verifyJwt(token,jwtSecret);
-		// find user by id
-		const user = await User.findById(id);
-		if(!user){
-			const error = new Error('unauthorized');
-			error.statusCode = 401;
-			return next(error)
-		}
-		//  attach user to request body
-		req.user = user;
-		next();	
-	}catch(err){
-		console.log(err)
-		next(err)
-	}
+    try{
+        // extract token from headers
+        const token = req.headers.authorization;
+        if(!token){
+            // if token is not provided, throw error with status code 401
+            const error = new Error('unauthorized');
+            error.statusCode = 401;
+            return next(error)
+        }
+        // verify the token using jwtSecret
+        const {id} = await verifyJwt(token,jwtSecret);
+        // find user by id
+        const user = await User.findById(id);
+        if(!user){
+            // if user is not found, throw error with status code 401
+            const error = new Error('unauthorized');
+            error.statusCode = 401;
+            return next(error)
+        }
+        // attach user to request body
+        req.user = user;
+        next(); 
+    }catch(err){
+        console.log(err)
+        next(err)
+    }
 }
-// Grant access to specific roles
+
+// authorize specific roles to access certain routes
 exports.authorize = (...roles) => {
-	return (req, res, next) => {
-	  if (!roles.includes(req.user.role)) {
-		return next(
-		  new customError(
-			`User role ${req.user.role} is not authorized to access this route`,
-			403
-		  )
-		);
-	  }
-	  next();
-	};
+    return (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+        // if user role is not authorized, throw an error with status code 403
+        return next(
+          new customError(
+            `User role ${req.user.role} is not authorized to access this route`,
+            403
+          )
+        );
+      }
+      next();
+    };
 };
